d30e63e00ffa887aa36e057d160cec399414d835
MutaiAllan <MutaiAllan>
2023-08-17 13:19:13 +0300

    Done with list comprehensions and generator expressions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = []
+    for num in num_list:
+        if num % 2 == 0:
+            evens.append(num)
+    
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = []
+    for sentence in sentence_list:
+        new_list.append(f'{sentence}!')
+    
+    return new_list
+    
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
